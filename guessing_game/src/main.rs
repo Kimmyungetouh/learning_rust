8deb8f813542fb0e8089ac82ced63a9206d610a2
myunggodwin <myunggodwin@gmail.com>
2022-11-22 10:55:05 +0100

    guessing_game: working no looping

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,20 @@
 use std::io;
-use rand::R
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
     println!("Guess the number !");
+    println!("Choosing a secret number ...");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Please in put yout guess.");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line");
+    let guess: u32 = guess.trim().parse().expect("Please enter a number !");
     println!("You guessed : {}!\n", guess);
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small !"),
+        Ordering::Equal => println!("Well done !"),
+        Ordering::Greater => println!("Too big !")
+    }
+    println!("The secret number is {secret_number}")
 }
