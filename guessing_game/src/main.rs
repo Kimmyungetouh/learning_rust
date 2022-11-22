042007e771c11c67656f5756e16684810658a16a
myunggodwin <myunggodwin@gmail.com>
2022-11-22 11:29:19 +0100

    guessing_game: looping

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,15 +6,22 @@ fn main() {
     println!("Guess the number !");
     println!("Choosing a secret number ...");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("Please in put yout guess.");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Please enter a number !");
-    println!("You guessed : {}!\n", guess);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small !"),
-        Ordering::Equal => println!("Well done !"),
-        Ordering::Greater => println!("Too big !")
+    loop {
+        println!("Please in put yout guess.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please enter a number !");
+        println!("You guessed : {}!\n", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small !"),
+            Ordering::Greater => println!("Too big !"),
+            Ordering::Equal =>{
+                println!("Well done ! The secret number is {secret_number}");
+                break;
+            }
+
+        }
     }
-    println!("The secret number is {secret_number}")
+
+    // println!("The secret number is {secret_number}")
 }
