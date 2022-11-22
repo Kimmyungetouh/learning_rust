47fd792f9531ecf58823777ae429d9cef225bedb
myunggodwin <myunggodwin@gmail.com>
2022-11-22 11:44:57 +0100

    guessing_game: Error handled

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,22 +6,26 @@ fn main() {
     println!("Guess the number !");
     println!("Choosing a secret number ...");
     let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut attempts : u32 = 0;
     loop {
-        println!("Please in put yout guess.");
+        println!("Please input your guess.");
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("Failed to read line");
-        let guess: u32 = guess.trim().parse().expect("Please enter a number !");
-        println!("You guessed : {}!\n", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+        attempts += 1;
+        // println!("You guessed : {}!\n", guess);
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small !"),
-            Ordering::Greater => println!("Too big !"),
+            Ordering::Less => println!("\n *** TOO SMALL ! *** \n"),
+            Ordering::Greater => println!("\n*** TOO BIG ! *** \n"),
             Ordering::Equal =>{
-                println!("Well done ! The secret number is {secret_number}");
+                println!(
+                    "Well done ! The secret number is {secret_number} You guessed in {attempts} attempts !");
                 break;
             }
 
         }
     }
-
-    // println!("The secret number is {secret_number}")
 }
